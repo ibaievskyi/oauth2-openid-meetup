e46987c40142db6d78eab4a95fcae4dce2ab85b4
Illia Baievskyi <illia_baievskyi@epam.com>
2020-01-26 22:00:54 +0200

    Github client

---
+++ src/main/java/com/example/oauth2client/Oauth2ClientApplication.java
@@ -0,0 +1,32 @@
+package com.example.oauth2client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@SpringBootApplication
+public class Oauth2ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ClientApplication.class, args);
+    }
+}
+
+@RestController
+class IndexController{
+
+    @GetMapping("/")
+    public String index(@AuthenticationPrincipal OAuth2User user){
+        return "<h1>Hello, " + user.getAttributes().get("login") + "</h1>";
+    }
+
+    @GetMapping("user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User user){
+        return user.getAttributes();
+    }
+}
