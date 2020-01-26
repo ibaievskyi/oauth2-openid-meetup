0d0b454bff4e299f8265abc67f791fa7b1ab8d6d
Illia Baievskyi <illia_baievskyi@epam.com>
2020-01-27 01:34:26 +0200

    Authorities mapping

--- src/main/java/com/example/oauth2client/Oauth2ClientApplication.java
+++ src/main/java/com/example/oauth2client/Oauth2ClientApplication.java
@@ -3,17 +3,27 @@ package com.example.oauth2client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Oauth2ClientApplication {
@@ -21,46 +31,72 @@ public class Oauth2ClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(Oauth2ClientApplication.class, args);
     }
+}
 
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository(){
-        return new InMemoryClientRegistrationRepository(auth0());
+
+@Configuration
+@EnableWebSecurity
+class Oauth2Configuration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/create-message").hasAuthority("CREATE_MESSAGE")
+                .mvcMatchers("/delete-message").hasAuthority("DELETE_MESSAGE")
+                .mvcMatchers("/manage-message").hasAuthority("MANAGE_MESSAGE")
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint().userAuthoritiesMapper(getGrantedAuthoritiesMapper());
+
     }
 
-    private ClientRegistration auth0(){
-        return ClientRegistration.withRegistrationId("auth0")
-                .clientName("OAuth2 client")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .clientId("mTRDwuz9sT3GkkA8SHf2pt4eNHXqnpYB")
-                .clientSecret("Oc0TFAqcGLVdBJBebCqEqIzy9Rnvc1PsSgTPrDuh4-cDVhWhKjkcL07ZAQze62bk")
-                .scope("openid", "profile", "email")
-                .redirectUriTemplate("{baseUrl}/login/oauth2/code/{registrationId}")
+    private GrantedAuthoritiesMapper getGrantedAuthoritiesMapper() {
+        String namespace = "http://localhost/";
+        String attributeName = namespace + "authorities";
+        String issuer = "https://activx.auth0.com/";
 
-                .authorizationUri("https://activx.auth0.com/authorize")
-                .tokenUri("https://activx.auth0.com/oauth/token")
-                .userInfoUri("https://activx.auth0.com/userinfo")
-                .userNameAttributeName("sub")
-                .jwkSetUri("https://activx.auth0.com/.well-known/jwks.json")
-                .build();
+        return authorities ->
+                authorities.stream()
+                        .filter(OidcUserAuthority.class::isInstance)
+                        .map(OidcUserAuthority.class::cast)
+//                        .filter(userAuthority -> userAuthority.getIdToken().getIssuer().toString().equals(issuer))
+//                        .filter(userAuthority -> userAuthority.getIdToken().containsClaim(attributeName))
+                        .map(userAuthority -> userAuthority.getIdToken().getClaimAsStringList(attributeName))
+                        .flatMap(Collection::stream)
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toCollection(LinkedHashSet::new));
     }
-
-
-
 }
 
 
 @RestController
-class IndexController{
+class IndexController {
 
     @GetMapping("/")
-    public String index(@AuthenticationPrincipal OAuth2User user){
+    public String index(@AuthenticationPrincipal OAuth2User user) {
         return "<h1>Hello, " + user.getAttributes().get("nickname") + "</h1>";
     }
 
     @GetMapping("user")
-    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User user){
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User user) {
         return user.getAttributes();
     }
+
+    @GetMapping("/delete-message")
+    public String delete() {
+        return "<h1>DELETED</h1>";
+    }
+
+    @GetMapping("/create-message")
+    public String create() {
+        return "<h1>created</h1>";
+    }
+
+    @GetMapping("/manage-message")
+    public String manage() {
+        return "<h1>managed</h1>";
+    }
+
 
 }
