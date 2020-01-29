cb9fc929a86d584031051802891c0753ce61dbc3
Illia Baievskyi <illia_baievskyi@epam.com>
2020-01-29 14:22:12 +0200

    comments

--- src/main/java/com/example/oauth2client/IndexController.java
+++ src/main/java/com/example/oauth2client/IndexController.java
@@ -34,6 +34,19 @@ public class IndexController{
         return user.getAttributes();
     }
 
+    /**
+     * Ендпоинт для получения с Resource server объекта  user info. Данные с ендпоинт userinfo можно получить
+     * только имея Access token, что в свою очередь идентично получению любых данных с Resource server.
+     * Сам урл для этого объекта мы получаем из конфигурации OAuth.
+     *
+     * @param client - в данном случае это авторизованный клиент
+     *               (ассоциация client registration + аутентифицированный юзер)
+     *               посмотрите из чего состоит этот объект будет полезно,
+     *               так же этот объект другим способом мы получаем в конфигурации для рест темплейта
+     *               {@link RestTemplateConfig}
+     *
+     * @return возвращает сущность описаную ниже
+     */
     @GetMapping("external")
     public ExternalUserInfo getExternal(@RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient client){
 
@@ -48,6 +61,13 @@ public class IndexController{
         return response.getBody();
     }
 
+    /**
+     * Entity - необходима для того чтобы получать данные с Resource Service
+     * мы знаем какие поля класс т.к. эта же информация представлена в idToken но мы будем получать
+     * эти данные с user-uri с Auth0
+     *
+     * Используется в контроллере выше
+     */
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
--- src/main/java/com/example/oauth2client/RestTemplateConfig.java
+++ src/main/java/com/example/oauth2client/RestTemplateConfig.java
@@ -9,24 +9,46 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestTemplate;
 
+
+/**
+ * Здесь мы создаем бин нашего RestTemplate для выполнения запроса к ресурс серверу.
+ */
 @RequiredArgsConstructor
 @Configuration
 public class RestTemplateConfig {
 
+    /**
+     * Бин рест темплейта
+     * @param oAuth2AuthorizedClientService - автовайрим сервис который знает откуда достать ассоциацию
+     *                                      аутентификации и конфига OAuth
+     *                                      ({@link OAuth2AuthorizedClientRepository - это хранилище которое использует под капотом сервис})
+     * @return рест темплейт
+     */
     @Bean
     public RestTemplate restTemplate(OAuth2AuthorizedClientService oAuth2AuthorizedClientService) {
-        return new RestTemplateBuilder().interceptors((httpRequest, bytes, execution) -> {
-            OAuth2AuthenticationToken auth = (OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        return new RestTemplateBuilder()
+                // Добавляем интерцептор, который в свою очередь будет добавлять Access Token к каждому
+                // реквесту, сделанному с помощью этого RestTemplate
 
-            OAuth2AuthorizedClient authorizedClient = oAuth2AuthorizedClientService.loadAuthorizedClient(
-                    auth.getAuthorizedClientRegistrationId(),
-                    auth.getName());
+                .interceptors((httpRequest, bytes, execution) -> {
 
-            httpRequest.getHeaders().add(HttpHeaders.AUTHORIZATION, "Bearer " + authorizedClient.getAccessToken().getTokenValue());
+                    //Достаем объект аутентификации текущий
+                    OAuth2AuthenticationToken auth = (OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
 
-            return execution.execute(httpRequest, bytes);
-        }).build();
+
+                    //Загружаем с ассоциацию нашей аутентификации с параметрами ClientRegistration(OAuth конфиг)
+                    // Делаем это чтобы достать  Access Token так как в объекте аутентификации он не хранится
+                    OAuth2AuthorizedClient authorizedClient = oAuth2AuthorizedClientService.loadAuthorizedClient(
+                            auth.getAuthorizedClientRegistrationId(),
+                            auth.getName());
+
+                    // Добавляем токен
+                    httpRequest.getHeaders().add(HttpHeaders.AUTHORIZATION, "Bearer " + authorizedClient.getAccessToken().getTokenValue());
+
+                    return execution.execute(httpRequest, bytes);
+                }).build();
     }
 }
